30fa6d2167b167a3c9971bb9c469e06e74a52ef0
Christopher Yuman <chrisyumanv@gmail.com>
2023-11-24 15:46:34 -0600

    Ejercicio03 resuelto

--- PAT_HojaDeTrabajo03/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio03.cpp
@@ -1,6 +1,37 @@
 #include "Ejercicio03.h"
 
-Node<int>* Ejercicio03::copyList(Node<int>* head)
-{
-    return nullptr;
+Node<int>* Ejercicio03::copyList(Node<int>* head) {
+    if (!head) {
+        return nullptr;
+    }
+
+    Node<int>* current = head;
+    while (current) {
+        Node<int>* duplicate = new Node<int>{ current->value, nullptr, nullptr };
+        duplicate->next = current->next;
+        current->next = duplicate;
+        current = duplicate->next;
+    }
+
+    current = head;
+    while (current) {
+        if (current->random) {
+            current->next->random = current->random->next;
+        }
+        current = current->next->next;
+    }
+
+    Node<int>* newHead = head->next;
+    current = head;
+    Node<int>* copyCurrent = newHead;
+    while (current) {
+        current->next = copyCurrent->next;
+        current = current->next;
+        if (current) {
+            copyCurrent->next = current->next;
+            copyCurrent = copyCurrent->next;
+        }
+    }
+
+    return newHead;
 }
