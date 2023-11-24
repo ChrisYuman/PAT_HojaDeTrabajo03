7acfd84e8770162db6edc02e2631f30eafdb787b
Christopher Yuman <chrisyumanv@gmail.com>
2023-11-24 15:08:25 -0600

    Ejercicio 1, resuelto

--- PAT_HojaDeTrabajo03/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio01.cpp
@@ -2,5 +2,18 @@
 
 int Ejercicio01::findMin(int nums[], int n)
 {
-    return 0;
+    int izquierda = 0;
+    int derecha = n - 1;
+
+    while (izquierda < derecha) {
+        int mid = izquierda + (derecha - izquierda) / 2;
+        if (nums[mid] > nums[derecha]) {
+            izquierda = mid + 1;
+        }
+        else {        
+            derecha = mid;
+        }
+    }
+
+    return nums[izquierda];
 }
